47e3858a3043c781a9db7a0209380720afcd335f
Lemon <852051926@qq.com>
2019-12-13 16:21:53 +0800

    first

---
+++ src/main/java/com/xn/springclouddemo01/Springclouddemo01Application.java
@@ -0,0 +1,13 @@
+package com.xn.springclouddemo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springclouddemo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springclouddemo01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/xn/springclouddemo01/Springclouddemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xn.springclouddemo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springclouddemo01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
